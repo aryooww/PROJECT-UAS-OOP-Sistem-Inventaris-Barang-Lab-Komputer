9e220123227b04824ceeeba98d1bc6a79fe26ffb
aryooww <setyoaryowiguno@gmail.com>
2026-05-25 21:12:08 +0700

    membuat getter dan setter barang.java

--- model/barang.java
+++ model/barang.java
@@ -13,6 +13,51 @@ public abstract class barang {
         this.namaAlat = namaAlat;
         this.kodeBarang = kodeBarang;
         this.jumlahBarang = jumlahBarang;
+        this.kondisiBarang = kondisiBarang;
         this.kategoriBarang = kategoriBarang;
     }
+
+    // getter
+    public String getNamaAlat() {
+        return namaAlat;
+    }
+
+    public String getKodeBarang() {
+        return kodeBarang;
+    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+
+    public String getKondisiBarang() {
+        return kondisiBarang;
+    }
+
+    public String getKategoriBarang() {
+        return kategoriBarang;
+    }
+
+    // setter
+    public void setNamaAlat(String namaAlat) {
+        this.namaAlat = namaAlat;
+    }
+
+    public void setKodeBarang(String kodeBarang) {
+        this.kodeBarang = kodeBarang;
+    }
+
+    public void setJumlahBarang(int jumlahBarang) {
+        this.jumlahBarang = jumlahBarang;
+    }
+
+    public void setKondisiBarang(String kondisiBarang) {
+        this.kondisiBarang = kondisiBarang;
+    }
+
+    public void setKategoriBarang(String kategoriBarang) {
+        this.kategoriBarang = kategoriBarang;
+    }
+
+    
 }
