587b8f828e7541d71a8ff624b25d052e1bf0f11d
aryooww <setyoaryowiguno@gmail.com>
2026-05-25 21:08:55 +0700

    membuat abstract class barang dan constructornya

--- model/barang.java
+++ model/barang.java
@@ -1,5 +1,18 @@
 package model;
 
-public class barang {
-    
+// abstract class barang
+public abstract class barang {
+    private String namaAlat;
+    private String kodeBarang;
+    private int jumlahBarang;
+    private String kondisiBarang;
+    private String kategoriBarang;
+
+    // constructor
+    public barang(String namaAlat, String kodeBarang, int jumlahBarang, String kondisiBarang, String kategoriBarang) {
+        this.namaAlat = namaAlat;
+        this.kodeBarang = kodeBarang;
+        this.jumlahBarang = jumlahBarang;
+        this.kategoriBarang = kategoriBarang;
+    }
 }
